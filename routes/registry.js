7630ed7cd9050cc989068e7d67757f6d59c27d21
romablog <iromablog@gmail.com>
2016-02-11 14:40:14 +0300

    commit

--- routes/registry.js
+++ routes/registry.js
@@ -2,20 +2,23 @@ var Model = require('../models/model.js').Model;
 var uuid = require('uuid');
 
 exports.post = function(req, res, next) {
-    console.log(req.body);
     var username = req.body.username;
     var password = req.body.password;
     var email = req.body.email;
     var surname = req.body.surname;
     var about = req.body.about;
+    var theme = req.body.theme;
+    var language = req.body.language;
     Model.User.create({
         authId: uuid.v1(),
-        theme: 'light',
-        language: 'EN',
+        theme: theme,
+        language: language,
         firstName: username,
         lastName: surname,
         email: email,
-        about: about
+        about: about,
+        password: password
+    }). then(function(user){
+        res.send(user)
     });
-    res.sendStatus(200);
 };
